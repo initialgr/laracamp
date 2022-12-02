68ea975abed4fbccf10e017352dde34f9972bfc2
gilang <initial.gr.101@gmail.com>
2022-12-02 18:50:39 +0700

    install and integrate socialite

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,6 +21,21 @@
                      <a class="nav-link" href="#">Business</a>
                  </li>
              </ul>
+             @auth
+             <div class="user-logged dropdown">
+                 <a href="#">
+                     Halo, {{ Auth::user()->name }}!</a>
+                 <img src="{{ Auth::user()->avatar }}" class="user-photo" alt="">
+                 <div class="dropdown-content">
+                     <a href="#">My Dashboard</a>
+                     <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Sign Out</a>
+                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none">
+                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                     </form>
+                 </div>
+
+             </div>
+             @else
              <div class="d-flex">
                  <a href="{{ route('login') }}" class="btn btn-master btn-secondary me-3">
                      Sign In
@@ -30,5 +45,57 @@
                  </a>
              </div>
          </div>
+         @endauth
      </div>
  </nav>
+
+ <style>
+     /* Style The Dropdown Button */
+     .dropbtn {
+         background-color: #4CAF50;
+         color: white;
+         padding: 16px;
+         font-size: 16px;
+         border: none;
+         cursor: pointer;
+     }
+
+     /* The container <div> - needed to position the dropdown content */
+     .dropdown {
+         position: relative;
+         display: inline-block;
+     }
+
+     /* Dropdown Content (Hidden by Default) */
+     .dropdown-content {
+         display: none;
+         position: absolute;
+         background-color: #f9f9f9;
+         min-width: 160px;
+         box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+         z-index: 1;
+     }
+
+     /* Links inside the dropdown */
+     .dropdown-content a {
+         color: black;
+         padding: 12px 16px;
+         text-decoration: none;
+         display: block;
+     }
+
+     /* Change color of dropdown links on hover */
+     .dropdown-content a:hover {
+         background-color: #f1f1f1
+     }
+
+     /* Show the dropdown menu on hover */
+     .dropdown:hover .dropdown-content {
+         display: block;
+     }
+
+     /* Change the background color of the dropdown button when the dropdown content is shown */
+     .dropdown:hover .dropbtn {
+         background-color: #3e8e41;
+     }
+ </style>
